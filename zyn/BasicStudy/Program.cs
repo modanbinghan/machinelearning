4092704949d9482434e449157a9c6b6b9534d85b
zhangyining128 <zhangyining128@outlook.com>
2021-04-21 17:11:13 +0800

    zyn study

---
+++ zyn/BasicStudy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
